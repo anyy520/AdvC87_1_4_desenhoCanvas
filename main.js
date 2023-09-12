8ece0916ae473d1b0a55baa58aea52c8e90b7ebd
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-09-12 17:05:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
-
+// Primeiro, obteremos o elemento canvas com sua id e o armazenaremos em uma variável “canvas”,
+// para que possamos manipular o canvas ao longo do código js.
 canvas = document.getElementById("myCanvas");
 
+// Depois, obteremos a referência do canvas com getContext("2d"). ● Esse será utilizado para desenhar
+// objetos/formas no canvas.
+// A variável ctx será utilizada como referência do canvas para desenhar.
 ctx= canvas.getContext("2d");
 
+// ajustaremos a variável color, e essa será utilizada para ajustar a cor que queremos aplicar ao nosso 
+//círculo ou a outras formas.
 color = "red"; 
 
+// ctx.anything, ‘anything’ pode ser qualquer método utilizado no canvas.
+// beginPath(): Esse inicia um caminho, ou reinicia o caminho atual para desenhar qualquer coisa. 
+//Ele pede ao cavans para iniciar o desenho do(a) objeto/forma
 ctx.beginPath();
+// strokeStyle: Esse ajustou a cor do objeto desenhado. ■ Ajustamos esse para a variável color. 
+//A variável Color continha red, isso significa que a cor do círculo era vermelha.
 ctx.strokeStyle = color;
+// ○ lineWidth: Esse ajustou a width para o objeto desenhado.
 ctx.lineWidth = 2;
+// arc: Esse é utilizado para criar um(a) arco/curva (utilizado para criar círculos, ou partes de um círculo).
+// 2 * Math.PI é o endAngle (ângulo final). ■ Utilizamos 2 * Math.PI, pois Math.PI é a metade do círculo
+// (pi é 180 graus). Logo, se quisermos o círculo completo, fazemos 2 * Math.PI.
 ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+// Sintaxe - arc(x, y, r, startAngle, endAngle); ○ x - A coordenada horizontal do centro do arco, ou seja, a 
+//coordenada x. ○ y - A coordenada vertical do centro do arco, ou seja, a coordenada y. ○ r - O raio do arco.
+// ○ startAngle - O ângulo em que o arco é iniciado, medido a partir do eixo x. ○ endAngle - O ângulo em que o 
+// arco é finalizado, medido a partir do eixo x.
 ctx.stroke();
 
-canvas.addEventListener("mousedown", my_mousedown);
+canvas.addEventListener("mousedown", myMousedown);
 
-function my_mousedown(e)
+function myMousedown(e)
 {
-    //pegando cor da caixa de entrada
+    //obtém a cor da caixa de inserção
     //início da atividade adicional
     color = document.getElementById("color").value;
     console.log(color);
     //fim da atividade adicional
     
-     mouse_x = e.clientX - canvas.offsetLeft;
-     mouse_y = e.clientY - canvas.offsetTop;
+    // e.clientX fornece as coordenadas x do cursor no canvas, quando pressionado
+    // após obtermos as coordenadas x, precisávamos armazená-las na variável mouseX.
+     mouseX = e.clientX - canvas.offsetLeft;
+    //  e.clientY fornece as coordenadas y no canvas, quando pressionado
+    //  após obtermos as coordenadas y, precisávamos armazená-las na variável mouseY..
+     mouseY = e.clientY - canvas.offsetTop;
 
-    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
-    circle(mouse_x , mouse_y);    
+    //  Então, imprimimos essas coordenadas no console para observarmos esse números.
+    console.log("X = " + mouseX + " ,Y =  " + mouseY);
+    circle(mouseX , mouseY);    
 }
 
-function circle(mouse_x , mouse_y)
+// Defina a função circle com as variáveis mouseX e mouseY, pois, queremos desenhar um círculo onde clicamos.
+function circle(mouseX , mouseY)
 {
 ctx.beginPath();
 ctx.strokeStyle = color;
 ctx.lineWidth = 2;
-ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+// ctx.arc(mouseX, mouseY, 40 ,0 , 2*Math.PI); ○mouseXserá a coordenada x em que o mouse clicou.
+// ○mouseYserá a coordenada y em que o mouse clicou ○40é o raio. ○0é o start angle. ○2 * Math.PI será o
+// end angle do círculo. ○stroke():Esse efetivamente desenha o caminho quevocê definiu .
+ctx.arc(mouseX ,mouseY, 40 ,0 , 2*Math.PI);
 ctx.stroke();
 }
 
